7c1121dd8fd5ce16e82275df89599bc96252152b
LazuardiMahendra <lazuardhie.mahendra@gmail.com>
2024-12-08 13:05:45 +0700

    first init

---
+++ app/src/main/java/com/example/stopwatchapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.stopwatchapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.stopwatchapp.databinding.ActivityMainBinding
+import me.tankery.lib.circularseekbar.CircularSeekBar
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        circularSeekBar()
+    }
+
+    private fun circularSeekBar() {
+        binding.csMain.setOnSeekBarChangeListener(object :
+            CircularSeekBar.OnCircularSeekBarChangeListener {
+            override fun onProgressChanged(
+                circularSeekBar: CircularSeekBar?,
+                progress: Float,
+                fromUser: Boolean
+            ) {
+                TODO("Not yet implemented")
+
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: CircularSeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seekBar: CircularSeekBar?) {
+                println("seek bar ${seekBar!!.progress}")
+            }
+
+        })
+    }
+
+}
